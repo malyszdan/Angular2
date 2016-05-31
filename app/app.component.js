ecc807134ac235acd6f9b24158fd0739a85ff89b
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2016-05-31 09:24:51 +0200

    add styles to component

--- app/app.component.js
+++ app/app.component.js
@@ -17,6 +17,7 @@ var Car = (function () {
 exports.Car = Car;
 var AppComponent = (function () {
     function AppComponent() {
+        this.cars = CARS;
         this.title = 'World of Cars';
         this.car = {
             id: 1,
@@ -26,11 +27,24 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>{{car.model}}</h2>\n    <div><label>id: </label>{{car.id}}</div>\n    <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"car.model\" placeholder=\"model\">\n    </div>\n\n    "
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Cars</h2>\n    <ul class=\"cars\">\n    <li *ngFor = \"let car of cars\" (click)=\"onSelect(car)\">\n    <span class=\"card\">{{car.id}}</span> {{car.model}}\n    <!-- each car goes here -->\n    </li>\n    </ul>\n    <h2>{{car.model}}</h2>\n    <div><label>id: </label>{{car.id}}</div>\n    <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"car.model\" placeholder=\"model\">\n    </div>\n\n    ",
+            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .cars {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .cars li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .cars li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .cars li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .cars .text {\n    position: relative;\n    top: -3px;\n  }\n  .cars .card {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var CARS = [
+    { "id": 11, "model": "WV Golf" },
+    { "id": 12, "model": "Audi A3" },
+    { "id": 13, "model": "Mercedes S" },
+    { "id": 14, "model": "Citroen" },
+    { "id": 15, "model": "Skoda" },
+    { "id": 16, "model": "BMW" },
+    { "id": 17, "model": "Audi" },
+    { "id": 18, "model": "Honda" },
+    { "id": 19, "model": "Toyota" },
+    { "id": 20, "model": "Fiat" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,13 @@ export class Car {
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
+    <h2>My Cars</h2>
+    <ul class="cars">
+    <li *ngFor = "let car of cars" (click)="onSelect(car)">
+    <span class="card">{{car.id}}</span> {{car.model}}
+    <!-- each car goes here -->
+    </li>
+    </ul>
     <h2>{{car.model}}</h2>
     <div><label>id: </label>{{car.id}}</div>
     <div>
@@ -16,12 +23,75 @@ export class Car {
     <input [(ngModel)]="car.model" placeholder="model">
     </div>
 
-    `
+    `,
+    styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .cars {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .cars li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .cars li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .cars li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .cars .text {
+    position: relative;
+    top: -3px;
+  }
+  .cars .card {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
+    public cars = CARS;
     title = 'World of Cars';
     car: Car = {
         id: 1,
         model: 'BMW M3'
     };
 }
+
+var CARS: Car[] = [
+    { "id": 11, "model": "WV Golf" },
+    { "id": 12, "model": "Audi A3" },
+    { "id": 13, "model": "Mercedes S" },
+    { "id": 14, "model": "Citroen" },
+    { "id": 15, "model": "Skoda" },
+    { "id": 16, "model": "BMW" },
+    { "id": 17, "model": "Audi" },
+    { "id": 18, "model": "Honda" },
+    { "id": 19, "model": "Toyota" },
+    { "id": 20, "model": "Fiat" }
+];
