12e5ce85327e00eb198ee5dfc3dab39274909660
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2016-05-31 14:42:23 +0200

    updtade car.service

--- app/car.service.ts
+++ app/car.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Car } from './Car';
 import { CARS } from './data-cars';
 
 @Injectable()
 export class CarService {
 
     getCars(){
-        return CARS;
+        return Promise.resolve(CARS);
     }
-
+    
 }
