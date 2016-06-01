efabfe3cd3fd404ca204c02a95473f2f6a2ae9ec
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2016-06-01 17:07:03 +0200

    add dashboard component with css and html templates

---
+++ app/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from './Car';
+import { CarService } from './car.service';
+import { Router } from '@angular/router-deprecated';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: 'app/dashboard.component.html',
+    styleUrls: ['app/dashboard.component.css']
+})
+
+export class DashboardComponent implements OnInit {
+    cars: Car[] = [];
+
+    constructor(
+        private router: Router,
+        private carService: CarService) {
+    }
+
+    ngOnInit(){
+        this.carService.getCars()
+            .then(cars => this.cars = cars.slice(1,5));
+    }
+
+    gotoDetail(car: Car){
+        let link = ['CarDetail', { id: car.id }];
+        this.router.navigate(link);
+    }
+}
