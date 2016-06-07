cb26268ee314e135af09b8b6c0aadc3ba0c2e386
Daniel M <malyszczuk.daniel@gmail.com>
2016-06-07 23:41:41 +0200

    Delete data-cars.ts

--- app/data-cars.ts
+++
@@ -1,8 +0,0 @@
-import { Car } from './Car';
-
-export var CARS: Car[] = [
-    { "id": 1, "brand": "WV", "model" : "Golf",  "fuelConsump" : 3.5},
-    { "id": 2, "brand": "BMW", "model" : "M3",  "fuelConsump" : 4.5 },
-    { "id": 3, "brand": "AUDI", "model" : "A8",  "fuelConsump" : 7.5 },
-
-];
